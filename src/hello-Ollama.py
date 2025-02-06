f4e6beafe1098574ef91eaa281f7a33d4ea62b8e
Rodrigo Luiz Soares <rodrigolsoares@gmail.com>
2025-02-06 18:39:49 -0300

    Implementação RAG Estudo

--- src/hello-Ollama.py
+++ src/hello-Ollama.py
@@ -1,25 +1,8 @@
-#print("Teste")
-
-from langchain_huggingface import ChatHuggingFace
-from langchain_community.llms import HuggingFaceHub
 from langchain_ollama import ChatOllama
-from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
-
-# Exemplo com Hugging Face
-llm = HuggingFaceHub(
-    repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
-    model_kwargs={
-        "temperature": 0.1,
-        "return_full_text": False,
-        "max_new_tokens": 512,
-        #"stop": ["<|eot_id|>"],
-        # demais parâmetros que desejar
-    }
-)
 
 system_prompt = "Você é um assistente prestativo e está respondendo perguntas gerais."
 user_prompt = "{input}"
@@ -31,15 +14,7 @@ prompt = ChatPromptTemplate.from_messages([
     ("user", user_prompt + token_e)
 ])
 
-chain = prompt | llm
-
 input = "Explique para mim em até 1 parágrafo o conceito de redes neurais, de forma clara e objetiva"
-
-res = chain.invoke({"input": input})
-print(res)
-print("-----")
-
-### Exemplo com Ollama
 
 llm = ChatOllama(
     model="phi3",
